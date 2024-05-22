3c9fef26929e5256c54f76febeed0f2b6a68372f
BAGUS MANUTO MUHAMMAD <134299193+manutommmmmmmmmmmmmmmmmm@users.noreply.github.com>
2024-05-22 17:02:00 +0700

    Add files via upload

---
+++ KIRANA DESAIN INDONESIA/js/script.js
@@ -0,0 +1,16 @@
+// toggle class active 
+
+const navbarNav = document.querySelector
+('.navbar-nav');
+document.querySelector('#hamburger-menu').onclick = () => {
+    navbarNav.classList.toggle('active');
+};
+ // click diluar sidebar 
+
+ const hamburger = document.querySelector('#hamburger-menu');
+
+ document.addEventListener('click', function(e) {
+    if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {(
+        navbarNav.classList.remove('active')
+    )
+}})
